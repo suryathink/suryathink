51a2f809a6f8ae62fd82ce30c343f0ff3649f424
Surya Prakash Singh <suryaprakashsingh110@gmail.com>
2023-03-01 12:34:22 +0530

    index updated

--- index.js
+++ index.js
@@ -1,42 +1,5 @@
-// $(document).ready(function(){
 
-//     $('#menu').click(function(){
-//       $(this).toggleClass('fa-times');
-//       $('header').toggleClass('toggle');
-//     });
-  
-//     $(window).on('scroll load',function(){
-  
-//       $('#menu').removeClass('fa-times');
-//       $('header').removeClass('toggle');
-  
-//       if($(window).scrollTop() > 0){
-//         $('.top').show();
-//       }else{
-//         $('.top').hide();
-//       }
-  
-//     });
-  
-//     // smooth scrolling 
-  
-//     $('a[href*="#"]').on('click',function(e){
-  
-//       e.preventDefault();
-  
-//       $('html, body').animate({
-  
-//         scrollTop : $($(this).attr('href')).offset().top,
-  
-//       },
-//         500, 
-//         'linear'
-//       );
-  
-//     });
-  
-//   });
-  let sidemenu = document.getElementById("SideBar");
+let sidemenu = document.getElementById("SideBar");
 
 const openMenu = () => {
 	sidemenu.style.right = "0";
@@ -46,44 +9,7 @@ const closeMenu = () => {
 	sidemenu.style.right = "-200px";
 }
 
-  // const Nav = () => {
-  //   return `<nav class="nav" role="navigation">
-      
-  //   // <h3 class="logo" id="change">Portfo<span>lio</span></h3>
-  //   <ul class="nav__items" id="SideBar">
-  //     <li class="nav__item">
-  //       <a href="#top" class="nav__link">Home</a>
-  //     </li>
-  //     <li class="nav__item">
-  //       <a href="#About" class="nav__link">About</a>
-  //     </li>
-  
-  //     <li class="nav__item">
-  //     <a href="./img/Krishna Keshav Resume.pdf" download="Krishna Keshav Resume" title="Download Resume" class="nav__link">Resume</a>
-  //     </li>
-      
-  //     <li class="nav__item">
-  //       <a href="#Skills" class="nav__link">Skills</a>
-  //     </li>
-  //     <li class="nav__item">
-  //       <a href="#projects" class="nav__link">Projects</a>
-  //     </li>
-  //     <li class="nav__item">
-  //       <a href="#contact" class="nav__link">Contact</a>
-  //     </li>
-  
-  //     <li class="nav__item">
-  //       <a href="#Git" class="nav__link">Contribution</a>
-  //     </li>
-      
-  
-  //         <i class="fa-sharp fa-solid fa-xmark" onclick="closeMenu()"></i>
-  //   </ul>
-  //     <i class="fa-solid fa-bars" onclick="openMenu()"></i>
-  // </nav>`
-  // }
-
-  // document.querySelector("#nav").innerHTML = Nav();
+ 
   
   const skills = () => {
 
@@ -92,7 +18,7 @@ const closeMenu = () => {
     <h1 class="heading"> <span>My</span>Skills</h1>
     <div class="Myslills">
       <ul class="Thechical">
-        <li><img src="./img/Java-PNG-Clipart.png" class="project__logo" alt="Java" /></li>
+        <li><img src="./img/Reactlogo.png" class="project__logo" alt="React" /></li>
         <li><img src="./img/spring.png" class="project__logo" alt="Springboot" /></li>
         <li><img src="./img/mysql.png" class="project__logo" alt="MySql" /></li>
         <li><img src="./img/hibernate.png" class="project__logo" alt="Hibernate" /></li>
@@ -133,7 +59,7 @@ document.getElementById("resume_button_2").addEventListener("click", () => {
 
   const Href = () => {
     let Src = [
-      "./img/Java-PNG-Clipart.png",
+      "./img/Reactlogo.png",
       "./img/js-logo.png",
       "./img/spring.png",
       "./img/hibernate.png",
@@ -147,7 +73,7 @@ document.getElementById("resume_button_2").addEventListener("click", () => {
   };
   
   const TechStack = () => {
-    let skills = ["Java", "Mysql", "Spring boot", "JavaScript", "DSA", "Hibernate", "HTML", "CSS", "Git",]
+    let skills = ["React", "Mysql", "Spring boot", "JavaScript", "DSA", "Hibernate", "HTML", "CSS", "Git",]
     return skills;
   }
   const len = TechStack();
